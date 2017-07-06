34be77e6f4b8ebd894330571571df8ea4aa09e96
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-07-06 08:00:46 +0200

    Add property test for progress theorem for System T

--- T.rkt
+++ T.rkt
@@ -7,7 +7,7 @@
   (e ::= x
          z
          (s e)
-         (rec e_0 (x_1 x_2 e_1) e)
+         (rec e_0 (x_!_ x_!_ e_1) e)
          (lam t x e)
          (ap e_1 e_2))
   (Γ ::= • (x : t Γ))
@@ -43,7 +43,7 @@
    (types Γ (s e) nat)]
   [(types Γ e nat)
    (types Γ e_0 t)
-   (types (x_1 : nat x_2 : t Γ) e_1 t)
+   (types (x_1 : nat (x_2 : t Γ)) e_1 t)
    -----------------------------------
    (types Γ (rec e_0 (x_1 x_2 e_1) e) t)]
   [(types (x : t_1 Γ) e t_2)
@@ -66,6 +66,26 @@
 (module+ test
   (test-->> red (term (ap (lam nat x (rec z (u v (s (s v))) x)) (s z))) (term (s (s z))))
   (test-->> red (term (ap (lam nat x (rec z (u v (s (s v))) x)) (s (s z)))) (term (s (s (s (s z)))))))
+
+(define v? (redex-match T v))
+
+(define (reduces? e)
+  (not (null? (apply-reduction-relation
+               red
+               (term ,e)))))
+
+(define (types? e)
+  (not (null? (judgment-holds (types • ,e t)
+                              t))))
+
+(define (progress-holds? e)
+  (if (types? e)
+      (or (v? e)
+          (reduces? e))
+      #t))
+
+(module+ test
+  (redex-check T e (progress-holds? (term e))))
 
 (module+ test
   (test-results))
